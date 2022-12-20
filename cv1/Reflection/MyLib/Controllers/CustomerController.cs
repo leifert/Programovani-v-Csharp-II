71a98915641cc185607ad3266c7cf24d03a7be32
Matěj Leifert <32055635+leifert@users.noreply.github.com>
2022-12-20 21:20:44 +0100

    Reflexe, assemblies

---
+++ cv1/Reflection/MyLib/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using MyLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.Controllers
+{
+    class CustomerController
+    {
+
+        private static List<Customer> customers = new List<Customer>()
+        {
+            new Customer(){ Id = 1, Age = 34, IsActive = true, Name = "Jan" },
+            new Customer(){ Id = 2, Age = 46, IsActive = false, Name = "Tom" },
+            new Customer(){ Id = 3, Age = 36, IsActive = true, Name = "Michala" }
+        };
+
+        public string List(int limit)
+        {
+            StringBuilder result = new StringBuilder();
+           
+            foreach(var customer in customers.Take(limit))
+            {
+                result.AppendLine(customer.Name);
+            }
+            return result.ToString();
+
+        }
+
+        public string Add(string name, int age, bool isActive) {
+            int id = customers.Select(x => x.Id).DefaultIfEmpty(0).Max() + 1;
+            customers.Add(new Customer()
+            {
+                Id = id,
+                Name = name,
+                Age = age,
+                IsActive = isActive
+            });
+            return id.ToString();
+        }
+
+        public string Add2(Customer customer)
+        {
+            int id = customers.Select(x => x.Id).DefaultIfEmpty(0).Max() + 1;
+            customer.Id = id;
+            customers.Add(customer);
+            return id.ToString();
+        }
+
+    }
+}
---
+++ cv1/Reflection/ReflectionApp/Program.cs
@@ -0,0 +1,125 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+
+
+
+namespace ReflectionApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            Run("/Customer/Add?Name=Pepa&Age=30&IsActive=true");
+            Run("/Customer/Add?Name=Pepa&Age=30&IsActive=true");
+        }
+
+
+        private static void Run(string url)
+        {
+            //string url = "/Customer/List?limit=2";
+            string[] mainParts = url.Split('?');
+            string[] pathParts = mainParts[0].Split('/');
+            string[] queryParts = mainParts[1].Split('&');
+
+            Dictionary<string, string> queryDict = queryParts
+                .Select(x => x.Split('='))
+                .ToDictionary(x =>x[0], y =>y[1], StringComparer.OrdinalIgnoreCase);
+
+            string controller = pathParts[1];
+            string action = pathParts[2];
+
+            string path = "../../../../MyLib/bin/Debug/net5.0/MyLib.dll";
+            path = Path.GetFullPath(path);
+
+            Assembly assembly = Assembly.LoadFile(path);
+
+            //foreach (Type type in assembly.GetTypes()) {
+               // Console.WriteLine(type.FullName);
+            //}
+
+            Type controllerType = assembly.GetType($"MyLib.Controllers.{controller}Controller");
+
+            //object instance1 = assembly.CreateInstance("MyLib.Controllers.CustomerController");
+            object instance = Activator.CreateInstance(controllerType);
+            //object instance3 = controllerType.Assembly.CreateInstance(controllerType.Name);
+
+            
+            MethodInfo myMethod = controllerType.GetMethod(action);
+
+            List<object> arguments = new List<object>();
+            foreach (ParameterInfo par in myMethod.GetParameters()) {
+
+                if (par.ParameterType.IsClass && par.ParameterType != typeof(string)) {
+
+                    object obj = Activator.CreateInstance(par.ParameterType);
+
+                    foreach (PropertyInfo prop in par.ParameterType.GetProperties()) {
+
+                        if (!queryDict.ContainsKey(prop.Name)) { continue; }
+
+                        string val2 = queryDict[prop.Name];
+                        if (prop.PropertyType == typeof(int))
+                        {
+                            prop.SetValue(obj, int.Parse(val2));
+                        }
+                        else if (prop.PropertyType == typeof(string))
+                        {
+                            prop.SetValue(obj, val2);
+                        }
+                        else if (prop.PropertyType == typeof(bool))
+                        {
+                            prop.SetValue(obj, bool.Parse(val2));
+                        }
+                        else
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        prop.SetValue(obj, val2);
+                    }
+
+                    //foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                    //{}
+
+                    arguments.Add(obj);
+                    continue;
+                }
+
+                string val = queryDict[par.Name];
+
+                if (par.ParameterType == typeof(int))
+                {
+                    arguments.Add(int.Parse(val));
+                }
+                else if (par.ParameterType == typeof(string))
+                {
+                    arguments.Add(val);
+                }
+                else if (par.ParameterType == typeof(bool))
+                {
+                    arguments.Add(bool.Parse(val));
+                }
+                else {
+                    throw new NotImplementedException();
+                }
+
+            }
+
+            object result = myMethod.Invoke(instance, arguments.ToArray());
+            Console.WriteLine(result);
+
+
+            
+            
+
+
+
+        }
+
+    }
+}
